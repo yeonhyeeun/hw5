28c8fc5e9b87f10c67cb4b7f46ca686e58760921
YeonHyeEun <21900442@handong.edu>
2021-07-09 16:39:51 +0900

    create hw5

---
+++ app/src/main/java/com/example/hw5/hw5.java
@@ -0,0 +1,14 @@
+package com.example.hw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hw5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ma5);
+    }
+}
